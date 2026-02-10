40d749eb306d10cd58d3aab87f34bf6b16254f5a
RodrigoBoehme <robi.boehme@gmail.com>
2026-02-09 21:48:16 -0300

    Alterção no arquivo pet.types.ts

--- api-pets-mvc-db/src/types/pet.types.ts
+++ api-pets-mvc-db/src/types/pet.types.ts
@@ -1,3 +1,11 @@
-export type pet={
-    
+//Tipo básico:Ajuda a padronizar os valores aceitos
+export type Especie="cachorro"|"gato"|"outro"
+//Modelo de dados do Pet (COMO EXISTE NO BANCO)
+export type Pet={
+    id:number
+    nome:string
+    especie:Especie
+    idade:number
+    tutor:string
+    criado_em?:string
 }
