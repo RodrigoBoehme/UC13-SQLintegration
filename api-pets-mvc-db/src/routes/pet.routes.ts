afedae5f38f992838b534b871187f56207949566
RodrigoBoehme <robi.boehme@gmail.com>
2026-02-10 21:50:27 -0300

    Adicionado routes a pasta src

---
+++ api-pets-mvc-db/src/routes/pet.routes.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+import { PetController } from "../controller/pet.controller";
+
+//Cria o "roteador"
+export const petRoutes=Router()
+/**
+ * Cada rota abaixo representa um endpoint da API
+ * Elas serão chamadas pelo fetch do front-end 
+ */
+
+petRoutes.get("/api/pets",PetController.listApi)
+//busca por id
+petRoutes.get("/api/pets/:id",PetController.getByIdApi)
+petRoutes.post("/api/pets",PetController.createApi)
+petRoutes.put("/api/pets/:id",PetController.updateApi)
+petRoutes.delete("/api/pet/:id",PetController.removeApi)
